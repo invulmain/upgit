73a60f2c005d6f0f21154f6d7bb25290f23ce0bd
invulmain <39603833+invulmain@users.noreply.github.com>
2018-06-27 22:34:20 +0700

    1

--- upgit.sh
+++ upgit.sh
@@ -15,6 +15,8 @@ echo -e "add08.sh:"
 cat /home/user/polaris/add08.sh | perl -ne 'print if 3..3'
 
 chmod +x /home/user/polaris/a.sh
-alias a='/home/user/polaris/a.sh'
+
+sed -i 's/.*# this, if it's already.*/alias a='\''\/home\/user\/polaris\/a.sh'\''/' .bashrc
+sed -i 's/.*# sources /etc/bash.*/alias re='\''\/hive\/opt\/opendev\/watchdog-opendev reset'\''/' .bashrc
 
 exit 0
