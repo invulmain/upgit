84b6ef6182ea3988b32475e45944f64128281e4c
invulmain <39603833+invulmain@users.noreply.github.com>
2018-06-27 20:05:47 +0700

    1

--- upgit.sh
+++ upgit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#ver 1
+#ver 2
 
 rm -R /home/user/polaris
 git clone https://github.com/invulmain/polaris /home/user/polaris
@@ -13,4 +13,8 @@ echo -e "add0.sh:"
 cat /home/user/polaris/add0.sh | perl -ne 'print if 3..3'
 echo -e "add08.sh:"
 cat /home/user/polaris/add08.sh | perl -ne 'print if 3..3'
+
+chmod +x /home/user/polaris/a.sh
+alias a='/home/user/polaris/a.sh'
+
 exit 0
